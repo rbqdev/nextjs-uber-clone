da0d15f77709bc1b396cb134e5b8dbb5c1d7e371
rbqdev <rbq.dev@gmail.com>
2024-03-04 10:33:39 -0300

    config: add prod app

--- src/configs/customNextServer/index.ts
+++ src/configs/customNextServer/index.ts
@@ -5,7 +5,7 @@ import { socketInit } from "../socket/server";
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "0.0.0.0";
 const port = Number(process.env.PORT!) || 10000;
-const app = next({ dev: true, dir: ".", hostname, port });
+const app = next({ dev, dir: ".", hostname, port });
 const handle = app.getRequestHandler();
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
   handle(req, res);
